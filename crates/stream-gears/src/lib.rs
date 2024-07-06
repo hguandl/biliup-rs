d04cc360a97d08fa1e911417689abf53df503499
Hao Guan <10684225+hguandl@users.noreply.github.com>
2024-07-06 22:33:46 +0800

    stream_gears: add upload2

--- crates/stream-gears/src/lib.rs
+++ crates/stream-gears/src/lib.rs
@@ -1,7 +1,9 @@
 mod login;
 mod uploader;
 
+use pyo3::marker::Ungil;
 use pyo3::prelude::*;
+use std::future::Future;
 use time::macros::format_description;
 use uploader::{PyCredit, StudioPre};
 
@@ -212,64 +214,32 @@ fn upload(
     dtime: Option<u32>,
     line: Option<UploadLine>,
 ) -> PyResult<()> {
-    py.allow_threads(|| {
-        let rt = tokio::runtime::Builder::new_current_thread()
-            .enable_all()
-            .build()?;
-        // 输出到控制台中
-        unsafe {
-            time::util::local_offset::set_soundness(time::util::local_offset::Soundness::Unsound);
-        }
-        let local_time = tracing_subscriber::fmt::time::LocalTime::new(format_description!(
-            "[year]-[month]-[day] [hour]:[minute]:[second]"
-        ));
-        let formatting_layer = tracing_subscriber::FmtSubscriber::builder()
-            // will be written to stdout.
-            // builds the subscriber.
-            .with_timer(local_time.clone())
-            .finish();
-        let file_appender = tracing_appender::rolling::never("", "upload.log");
-        let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
-        let file_layer = tracing_subscriber::fmt::layer()
-            .with_ansi(false)
-            .with_timer(local_time)
-            .with_writer(non_blocking);
-
-        let collector = formatting_layer.with(file_layer);
-
-        tracing::subscriber::with_default(collector, || -> PyResult<()> {
-            let studio_pre = StudioPre::builder()
-                .video_path(video_path)
-                .cookie_file(cookie_file)
-                .line(line)
-                .limit(limit)
-                .title(title)
-                .tid(tid)
-                .tag(tag)
-                .copyright(copyright)
-                .source(source)
-                .desc(desc)
-                .dynamic(dynamic)
-                .cover(cover)
-                .dtime(dtime)
-                .dolby(dolby)
-                .lossless_music(lossless_music)
-                .no_reprint(no_reprint)
-                .open_elec(open_elec)
-                .desc_v2_credit(desc_v2)
-                .build();
-
-            match rt.block_on(uploader::upload(studio_pre)) {
-                Ok(_) => Ok(()),
-                // Ok(_) => {  },
-                Err(err) => Err(pyo3::exceptions::PyRuntimeError::new_err(format!(
-                    "{}, {}",
-                    err.root_cause(),
-                    err
-                ))),
-            }
-        })
-    })
+    upload2(
+        py,
+        false,
+        video_path,
+        cookie_file,
+        title,
+        tid,
+        tag,
+        copyright,
+        source,
+        desc,
+        dynamic,
+        cover,
+        dolby,
+        lossless_music,
+        no_reprint,
+        open_elec,
+        false,
+        false,
+        false,
+        limit,
+        desc_v2,
+        dtime,
+        line,
+    )
+    .map(|_| ())
 }
 
 #[allow(clippy::too_many_arguments)]
@@ -299,6 +269,107 @@ fn upload_by_app(
     dtime: Option<u32>,
     line: Option<UploadLine>,
 ) -> PyResult<()> {
+    upload2(
+        py,
+        true,
+        video_path,
+        cookie_file,
+        title,
+        tid,
+        tag,
+        copyright,
+        source,
+        desc,
+        dynamic,
+        cover,
+        dolby,
+        lossless_music,
+        no_reprint,
+        open_elec,
+        up_close_reply,
+        up_selection_reply,
+        up_close_danmu,
+        limit,
+        desc_v2,
+        dtime,
+        line,
+    )
+    .map(|_| ())
+}
+
+#[allow(clippy::too_many_arguments)]
+#[pyfunction]
+fn upload2(
+    py: Python<'_>,
+    by_app: bool,
+    video_path: Vec<PathBuf>,
+    cookie_file: PathBuf,
+    title: String,
+    tid: u16,
+    tag: String,
+    copyright: u8,
+    source: String,
+    desc: String,
+    dynamic: String,
+    cover: String,
+    dolby: u8,
+    lossless_music: u8,
+    no_reprint: u8,
+    open_elec: u8,
+    up_close_reply: bool,
+    up_selection_reply: bool,
+    up_close_danmu: bool,
+    limit: usize,
+    desc_v2: Vec<PyCredit>,
+    dtime: Option<u32>,
+    line: Option<UploadLine>,
+) -> PyResult<String> {
+    spawn_logged_task(py, || async {
+        let studio_pre = StudioPre::builder()
+            .video_path(video_path)
+            .cookie_file(cookie_file)
+            .line(line)
+            .limit(limit)
+            .title(title)
+            .tid(tid)
+            .tag(tag)
+            .copyright(copyright)
+            .source(source)
+            .desc(desc)
+            .dynamic(dynamic)
+            .cover(cover)
+            .dtime(dtime)
+            .dolby(dolby)
+            .lossless_music(lossless_music)
+            .no_reprint(no_reprint)
+            .open_elec(open_elec)
+            .up_close_reply(up_close_reply)
+            .up_selection_reply(up_selection_reply)
+            .up_close_danmu(up_close_danmu)
+            .desc_v2_credit(desc_v2)
+            .build();
+
+        let response = match by_app {
+            true => uploader::upload_by_app(studio_pre).await,
+            false => uploader::upload(studio_pre).await,
+        };
+
+        match response {
+            Ok(value) => Ok(value.data.unwrap()["bvid"].as_str().unwrap().to_owned()),
+            Err(err) => Err(pyo3::exceptions::PyRuntimeError::new_err(format!(
+                "{}, {}",
+                err.root_cause(),
+                err
+            ))),
+        }
+    })
+}
+
+fn spawn_logged_task<T: Send, R, F>(py: Python<'_>, f: F) -> PyResult<T>
+where
+    R: Future<Output = PyResult<T>>,
+    F: Ungil + Send + FnOnce() -> R,
+{
     py.allow_threads(|| {
         let rt = tokio::runtime::Builder::new_current_thread()
             .enable_all()
@@ -324,41 +395,7 @@ fn upload_by_app(
 
         let collector = formatting_layer.with(file_layer);
 
-        tracing::subscriber::with_default(collector, || -> PyResult<()> {
-            let studio_pre = StudioPre::builder()
-                .video_path(video_path)
-                .cookie_file(cookie_file)
-                .line(line)
-                .limit(limit)
-                .title(title)
-                .tid(tid)
-                .tag(tag)
-                .copyright(copyright)
-                .source(source)
-                .desc(desc)
-                .dynamic(dynamic)
-                .cover(cover)
-                .dtime(dtime)
-                .dolby(dolby)
-                .lossless_music(lossless_music)
-                .no_reprint(no_reprint)
-                .open_elec(open_elec)
-                .up_close_reply(up_close_reply)
-                .up_selection_reply(up_selection_reply)
-                .up_close_danmu(up_close_danmu)
-                .desc_v2_credit(desc_v2)
-                .build();
-
-            match rt.block_on(uploader::upload_by_app(studio_pre)) {
-                Ok(_) => Ok(()),
-                // Ok(_) => {  },
-                Err(err) => Err(pyo3::exceptions::PyRuntimeError::new_err(format!(
-                    "{}, {}",
-                    err.root_cause(),
-                    err
-                ))),
-            }
-        })
+        tracing::subscriber::with_default(collector, || rt.block_on(f()))
     })
 }
 
@@ -372,6 +409,7 @@ fn stream_gears(m: &Bound<'_, PyModule>) -> PyResult<()> {
     //     .init();
     m.add_function(wrap_pyfunction!(upload, m)?)?;
     m.add_function(wrap_pyfunction!(upload_by_app, m)?)?;
+    m.add_function(wrap_pyfunction!(upload2, m)?)?;
     m.add_function(wrap_pyfunction!(download, m)?)?;
     m.add_function(wrap_pyfunction!(download_with_callback, m)?)?;
     m.add_function(wrap_pyfunction!(login_by_cookies, m)?)?;
